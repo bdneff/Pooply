a38659c0b7a2505593e8e0e742f9724566872bba
Grossyb <grossyb@yahoo.com>
2025-04-04 16:49:57 -0400

    Initial Commit

---
+++ Pooply/PooplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PooplyApp.swift
+//  Pooply
+//
+//  Created by Brandon Grossnickle on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PooplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
